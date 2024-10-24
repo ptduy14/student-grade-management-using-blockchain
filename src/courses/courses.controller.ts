1bd9264df5a90d606db3a3c962424a3d9ab71a44
ptduy14 <phantanduy14@gmail.com>
2024-10-24 20:44:13 +0700

    refactor: add guard for some method in course controller

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -30,11 +30,17 @@ export class CoursesController {
     return this.coursesService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(TeacherRoleEnum.ADMIN, TeacherRoleEnum.TEACHER)
   @Get(':id')
+  @ApiOperation({summary: 'Get course'})
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.coursesService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(TeacherRoleEnum.ADMIN)
+  @ApiOperation({summary: 'Update course'})
   @Patch(':id')
   update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateCourseDto: UpdateCourseDto) {
     return this.coursesService.update(id, updateCourseDto);
